01f630128605dba2f5a35862a6c221d69a6bf842
jarabeenoi <nongnat61@gmail.com>
2020-12-28 19:01:57 +0700

    landing-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Header from "./components/Header";
+
+const useStyles = makeStyles((theme) => ({
+   root: {
+      height: "100vh",
+      backgroundImage: `url("https://images.unsplash.com/photo-1528295674080-a70f6cbf4ff0?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fGp1bmdsZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80")`,
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover"
+   }
+}));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const classes = useStyles();
+
+   return (
+      <div className={classes.root}>
+         <Header/>
+      </div>
+   );
 }
 
 export default App;
